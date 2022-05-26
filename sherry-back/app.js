3c2170a0a1904649cf13df2fd1863c19cd97f823
David Padial <daviipadiall@gmail.com>
2022-05-26 12:22:38 +0200

    Prueba1

--- sherry-back/app.js
+++ sherry-back/app.js
@@ -1,51 +1,58 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
-require('dotenv').config();
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors");
+require("dotenv").config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var tarifasRouter = require('./routes/tarifas');
-var clasesRouter = require('./routes/clases');
-var historialRouter = require('./routes/historial');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var tarifasRouter = require("./routes/tarifas");
+var clasesRouter = require("./routes/clases");
+var historialRouter = require("./routes/historial");
 
-var db = require('./database');
+var db = require("./database");
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
 app.use(cors());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/tarifas', tarifasRouter);
-app.use('/clases', clasesRouter);
-app.use('/historial', historialRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/tarifas", tarifasRouter);
+app.use("/clases", clasesRouter);
+app.use("/historial", historialRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
+});
+
+app.use((req, res, next) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-Headers", "Content-Type");
+  res.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  next();
 });
 
 module.exports = app;
