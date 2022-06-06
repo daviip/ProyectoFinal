d30a62d5ad6f08c63c38b5c6efc69ce9d4b159ab
DavidPadial <david.padial@aktios.com>
2022-06-06 13:08:37 +0200

    Delete user

--- sherry-back/routes/users.js
+++ sherry-back/routes/users.js
@@ -66,10 +66,11 @@ router.post("/login", function (req, res, next) {
 
 // Eliminar usuario
 router.delete("/delete/:id", function (req, res, next) {
-  User.findByIdAndRemove(req.params.id, req.body, function (err, user) {
+  User.findByIdAndDelete(req.params.id, req.body, function (err, user) {
     if (err) return next(err);
     res.json(user);
   });
+  // User.findOneAndRemove
 });
 
 // Editar usuario
