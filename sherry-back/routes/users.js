1e44cffa10d1ac033a16c444bb46405a61450aa3
David Padial <daviipadiall@gmail.com>
2022-06-13 10:02:34 +0200

    e

--- sherry-back/routes/users.js
+++ sherry-back/routes/users.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-var User = require("../models/user");
+var User = require("../models/User");
 var bcrypt = require("bcryptjs");
 
 /* GET users listing. */
